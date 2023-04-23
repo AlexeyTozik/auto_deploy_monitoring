17b17d49f3878d02e81fcae1c56f673f97a6cde1
Alexis Bugatti <tozikalexey@gmail.com>
2023-04-23 14:32:25 +0300

    init: create simple To-Do list, using Flask & SQLite

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task_title = request.form['title']
+        new_task = Task(title=task_title)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ошибка добавления задачи'
+    else:
+        tasks = Task.query.order_by(Task.id).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Ошибка удаления задачи'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
